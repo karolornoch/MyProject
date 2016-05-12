cb67cfdb5a7c1dacb8465ef242992e0fcf5e533a
Karol Ornoch <karol.ornoch@gmail.com>
2016-05-12 22:53:55 +0200

    commit

--- Project/Models/Book.cs
+++ Project/Models/Book.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,17 @@ namespace Project.Models
     public class Book
     {
         public int ID { get; set; }
+        [Required]
+        [Display(Name = "Kategoria")]
         public int CategoryID { get; set; }
+        [Required]
+        [Display(Name = "Tytuł")]
         public string Title { get; set; }
+        [Required]
+        [Display(Name = "Autor")]
         public string Author { get; set; }
+        [Required]
+        [Display(Name = "Data Publikacji")]
         public int PublicationDate { get; set; }
         public virtual Category Category { get; set; }
 
